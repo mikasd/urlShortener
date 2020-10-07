b4902555e6ffc2c52013f0d2bf1e7d810d075b31
mikasd2 <test@pop-os.localdomain>
2020-10-06 22:30:16 -0700

    yaml handler works

--- handler.go
+++ handler.go
@@ -2,6 +2,8 @@ package urlshort
 
 import (
 	"net/http"
+	yaml "gopkg.in/yaml.v2"
+
 )
 
 // MapHandler will return an http.HandlerFunc (which also
@@ -38,5 +40,25 @@ func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.Handl
 // See MapHandler to create a similar http.HandlerFunc via
 // a mapping of paths to urls.
 func YAMLHandler(yml []byte, fallback http.Handler) (http.HandlerFunc, error) {
-	return nil, nil
+	var pathUrls []pathUrl
+
+	err := yaml.Unmarshal(yml, &pathUrls)
+	
+	if err != nil {
+		return nil, err 
+	}
+
+	pathsToUrls := make(map[string]string)
+	for _, pu := range pathUrls {
+		pathsToUrls[pu.Path] = pu.URL
+	}
+
+
+	
+	return MapHandler(pathsToUrls,fallback), nil
+}
+
+type pathUrl struct {
+	Path string `yaml:"path"` 
+	URL string `yaml:"url"`
 }
